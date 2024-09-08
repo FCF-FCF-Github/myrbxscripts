b8e4c12892ff2cb92097edabbfb7a98ce8e882fc
FCF-FCF-Github <139944260+FCF-FCF-Github@users.noreply.github.com>
2024-09-08 16:53:02 +0300

    Update and rename teleporttohalloween.lua to Easter.lua

--- Easter.lua
+++ Easter.lua
@@ -1,5 +1,4 @@
 local TeleportService = game:GetService ("TeleportService")
-local gamelD = 515498021
-local player =
-game.Players.LocalPlayer
+local gamelD = 1930863474
+local player = game.Players.LocalPlayer
 TeleportService:Teleport(gamelD, player)
